5639a45716ba5874f2c62c72c76189bbf68cbfde
Ritesh S Goriya <200120116018@git.org.in>
2023-08-21 09:38:09 +0530

    Commint

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar(props) {
       >
         <div className="container-fluid" style={{backgroundColor: props.mode==='dark'?'#02111d':'#dee2e6',
                     color: props.mode==='dark'?'#dee2e6':'black'}}>
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="#">
             {props.title}
           </a>
           <button
@@ -27,7 +27,7 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
